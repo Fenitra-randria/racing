8791adeb6ded06649f5dd7c51b1091576e0448e5
Fenitra-randria <93113077+Fenitra-randria@users.noreply.github.com>
2023-07-10 14:45:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Test de niveau 10 juillet 2023
+Jeux video Racing with treejs
---
+++ predeploy.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+:: copy all assets
+xcopy "./assets" "./dist/assets" /c /y
+xcopy "./assets/models" "./dist/assets/models" /c /y
+xcopy "./assets/sounds" "./dist/assets/sounds" /c /y
+xcopy "./assets/textures" "./dist/assets/textures" /c /y
+
+:: copy css
+xcopy "./css" "./dist/css" /c /y
